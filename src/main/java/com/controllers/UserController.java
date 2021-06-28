1a539a978465c201ded305630a9c0e6d419c574f
Ilham Saputrajati <ilhamsaputrajati@gmail.com>
2021-06-28 21:15:53 +0700

    add save user

--- src/main/java/com/controllers/UserController.java
+++ src/main/java/com/controllers/UserController.java
@@ -8,8 +8,11 @@ import com.repositories.UserRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -31,4 +34,10 @@ public class UserController {
                 .orElseThrow(() -> new ResourceNotFoundException("Employee not found for this id :: " + userId));
         return ResponseEntity.ok().body(user);
     }
+
+    @PostMapping("/users")
+    public User create(@Validated @RequestBody User employee) {
+        return repository.save(employee);
+    }
+
 }
